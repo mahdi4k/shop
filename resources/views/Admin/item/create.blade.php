5a253655082f7b598a48aa4086c1007116a40e63
horseman2k <falahati.mahdi@yahoo.com>
2019-05-01 09:08:21 +0430

    item products completed

---
+++ resources/views/Admin/item/create.blade.php
@@ -0,0 +1,81 @@
+@extends('Admin.master')
+
+@section('header')
+
+    افزودن ویژگی محصولات
+@endsection
+
+
+@section('content')
+
+    <div class="box_title">
+        <span>افزودن ویژگی محصولات</span>
+    </div>
+
+    @if(isset($id))
+        {!! Form::open(['url'=>'admin/item?id='.$id,'files'=>'true']) !!}
+    @endif
+
+    <div class="form-group">
+        {!! Form::label('cat_id','انتخاب سر دسته : ') !!}
+        {!! Form::select('cat[]',$cat_list,$id,['class'=>'selectpicker','data-live-search'=>'true','id'=>'cat_list','onchange'=>'get_item()']) !!}
+
+    </div>
+    <div class="form-group" id="item_box">
+
+
+        @foreach($item as $key=>$value)
+
+            <div id="item_div_{{ $value->id }}" style="width:100%;float:right;margin-top:10px;margin-bottom:5px" class="product_item_div">
+                <input name="item_name[{{ $value->id }}]" value="{{ $value->name }}" type="text"  style="float: right;" class="form-control" placeholder="نام گروه" >
+
+
+             @foreach($value->get_child as $key2=>$value2)
+
+                <div class="child_item">
+                    <input value="{{ $value2->name }}" type="text" name="child_item[{{ $value->id }}][{{ $value2->id }}]" style="float: right;" class="form-control" placeholder="نام آیتم">
+                    <select name="child_filed[{{ $value->id }}][{{ $value2->id }}]" class="form-control" style="float:right;margin-right:10px">
+                        <option @if($value2->filed==1) selected="selected" @endif  value="1">فیلد input</option>
+                        <option @if($value2->filed==2) selected="selected" @endif value="2">فیلد select</option>
+                        <option @if($value2->filed==3) selected="selected" @endif value="3">فیلد textarea</option>
+                        </select>
+                    </div>
+
+             @endforeach
+
+
+
+         </div>
+            <div class="form-group" style="margin-right:0px;margin-bottom:0px;">
+                <span class="fa fa-plus" style="color:red;cursor:pointer;padding-top:15px" onclick="add_child_item('item_div_{{ $value->id }}','{{ $value->id }}')"></span>
+            </div>
+        @endforeach
+
+    </div>
+    @if(isset($id))
+ 
+        <div class="form-group">
+
+            <span class="fa fa-plus" style="color:red;cursor:pointer;padding-top:15px" onclick="add_item()"></span>
+
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('ثبت',['class'=>'btn btn-success']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+    @endif
+    <script>
+        get_item=function ()
+        {
+            var cat_id=document.getElementById('cat_list').value;
+            window.location='<?= url('admin/item') ?>?id='+cat_id;
+        };
+    </script>
+
+
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,19 @@ Route::get('/', 'homeController@index');
 Route::get('/admin', 'Admin\PanelCotroller@index');
 
 //admin_category
-Route::resource('admin/category', 'admin\CategoryController', ['except' => ['show']]);
-Route::post('admin/category/del_img/{id}', 'admin\CategoryController@del_img');
+Route::resource('admin/category', 'Admin\CategoryController', ['except' => ['show']]);
+Route::post('admin/category/del_img/{id}', 'Admin\CategoryController@del_img');
 
 //admin_product
-Route::resource('admin/product', 'admin\ProductController', ['except' => ['show']]);
-Route::get('admin/product/gallery', 'admin\ProductController@gallery');
-Route::post('admin/product/upload', 'admin\ProductController@upload');
-Route::post('admin/product/del_product_img/{id}','admin\ProductController@del_product_img');
+Route::resource('admin/product', 'Admin\ProductController', ['except' => ['show']]);
+Route::get('admin/product/gallery', 'Admin\ProductController@gallery');
+Route::post('admin/product/upload', 'Admin\ProductController@upload');
+Route::post('admin/product/del_product_img/{id}','Admin\ProductController@del_product_img');
 
 //filter_product
-Route::get('admin/filter','admin\FilterController@index');
-Route::post('admin/filter','admin\FilterController@create');
+Route::get('admin/filter','Admin\FilterController@index');
+Route::post('admin/filter','Admin\FilterController@create');
+
+//item-section
+Route::get('admin/item','Admin\ItemController@index');
+Route::post('admin/item','Admin\ItemController@create');
